b470d023b6f63a2e6ae38c4778ce14850e6b7f9b
IzumiiHD (backup) <technoscootalo@gmail.com>
2026-05-24 01:18:42 -0400

    node fetch

--- backend/routers/api/packs.js
+++ backend/routers/api/packs.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const fetch = require("node-fetch");
-
 const Pack = require("../../models/Pack");
 const User = require("../../models/User");
 
