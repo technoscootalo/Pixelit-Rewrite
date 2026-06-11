0bee32fb9da0d015796ee4d45357f39e0217d3be
IzumiiHD <technoscootalo@gmail.com>
2026-06-10 20:15:33 -0400

    test

--- backend/models/StripePayment.js
+++ backend/models/StripePayment.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const StripePaymentSchema = new mongoose.Schema({
   sessionId: { type: String, required: true, unique: true },
+
   userId: { type: String },
+
   priceId: { type: String },
+
   fulfilled: { type: Boolean, default: false },
+  
   createdAt: { type: Date, default: Date.now }
 });
 
